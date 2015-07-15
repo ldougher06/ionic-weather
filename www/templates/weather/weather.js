2613cdba444821b01ac74afc9ae38190a3f69bd2
logan dougher <logan.dougher@gmail.com>
2015-07-14 19:42:03 -0500

    added loading icon

--- www/templates/weather/weather.js
+++ www/templates/weather/weather.js
@@ -1,10 +1,11 @@
 angular.module('won.weather', [])
-  .controller('WeatherCtrl', function($scope, $stateParams, $http){
+  .controller('WeatherCtrl', function($scope, $stateParams, $ionicLoading, $http){
     $scope.city = $stateParams.city;
-
+    $ionicLoading.show();
     $http
       .get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.long)
       .success(function(data){
         console.log(data);
+        $ionicLoading.hide();
       });
   });
